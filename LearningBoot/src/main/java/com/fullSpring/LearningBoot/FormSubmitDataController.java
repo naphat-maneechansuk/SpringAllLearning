8512bf968c1b1802e4bf88da10be708da1e2ed6d
naphat-maneechansuk <141388294+naphat-maneechansuk@users.noreply.github.com>
2025-07-24 15:16:11 +0700

    จบ Query Parameter

---
+++ LearningBoot/src/main/java/com/fullSpring/LearningBoot/FormSubmitDataController.java
@@ -0,0 +1,10 @@
+package com.fullSpring.LearningBoot;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FormSubmitDataController {
+    public String login(){
+        return "";
+    }
+}
