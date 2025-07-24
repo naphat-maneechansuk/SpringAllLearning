a6c50c93b1956312c3890729e0617b4bf07e45ea
naphat-maneechansuk <141388294+naphat-maneechansuk@users.noreply.github.com>
2025-07-24 14:08:44 +0700

    setUp

---
+++ LearningBoot/src/main/java/com/fullSpring/LearningBoot/LearningBootApplication.java
@@ -0,0 +1,13 @@
+package com.fullSpring.LearningBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningBootApplication.class, args);
+	}
+
+}
---
+++ LearningBoot/src/test/java/com/fullSpring/LearningBoot/LearningBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullSpring.LearningBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
