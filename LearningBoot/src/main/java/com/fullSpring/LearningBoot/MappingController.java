d3d9860049908bfba709a29a6eabdd5dd854a737
naphat-maneechansuk <141388294+naphat-maneechansuk@users.noreply.github.com>
2025-07-24 14:36:30 +0700

    จบ postman

--- LearningBoot/src/main/java/com/fullSpring/LearningBoot/MappingController.java
+++ LearningBoot/src/main/java/com/fullSpring/LearningBoot/MappingController.java
@@ -1,5 +1,25 @@
 package com.fullSpring.LearningBoot;
 
+import org.springframework.web.bind.annotation.*;
 
+@RestController
 public class MappingController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
+
+    @PutMapping("/hello2")
+    public String helloPut() {
+        return "Hello, World! This is a PUT request.";
+    }
+
+    @RequestMapping(value = "/hello3",method = {
+            RequestMethod.GET,
+            RequestMethod.POST
+    })
+    public String defaultMapping() {
+        return "This is a default mapping response.";
+    }
+
 }
