f0e7c8c80eea6bef474c73559b4bb390ca467099
naphat-maneechansuk <141388294+naphat-maneechansuk@users.noreply.github.com>
2025-07-25 17:41:22 +0700

    จบ download file

---
+++ LearningBoot/src/main/java/com/fullSpring/LearningBoot/controller/DawnloadFileController.java
@@ -0,0 +1,22 @@
+package com.fullSpring.LearningBoot.controller;
+
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.ByteArrayInputStream;
+
+@RestController
+public class DawnloadFileController {
+    @GetMapping("")
+    public ResponseEntity<InputStreamResource> downloadFile() {
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream("DATA IN FILE".getBytes());
+        InputStreamResource inputStreamResource = new InputStreamResource(byteArrayInputStream);
+        return ResponseEntity.ok()
+                .header("Content-Disposition", "attachment; filename=\"data.txt\"")
+                .contentType(MediaType.TEXT_PLAIN)
+                .body(inputStreamResource);
+    }
+}
