31312e9188cae40a0d23149e38a9ffe8d4d39c5e
naphat-maneechansuk <141388294+naphat-maneechansuk@users.noreply.github.com>
2025-07-31 13:23:54 +0700

    จบ update

--- LearningBoot/src/main/java/com/fullSpring/LearningBoot/TestJpaController.java
+++ LearningBoot/src/main/java/com/fullSpring/LearningBoot/TestJpaController.java
@@ -1,14 +1,11 @@
 package com.fullSpring.LearningBoot;
 
-import com.fullSpring.LearningBoot.dto.ShopDto;
 import com.fullSpring.LearningBoot.entity.Shop;
 import com.fullSpring.LearningBoot.repository.ShopRepositoy;
 import jakarta.persistence.Tuple;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 public class TestJpaController {
@@ -20,9 +17,14 @@ public class TestJpaController {
         return shopRepositoy.findById(id).orElse(null);
     }
 
-    @Transactional
+
     @PostMapping("jpa/insertShop")
     public Shop insertJpa(@RequestBody Shop shop) {
+        return shopRepositoy.save(shop);
+    }
+
+    @PostMapping("jpa/updateShop")
+    public Shop updateJpa(@RequestBody Shop shop) {
         return shopRepositoy.save(shop);
     }
 }
