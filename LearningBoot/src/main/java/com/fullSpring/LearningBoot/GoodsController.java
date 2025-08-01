049bc01c3ceb317553c77ee9f4e1e8b7ee0a7f8c
naphat-maneechansuk <141388294+naphat-maneechansuk@users.noreply.github.com>
2025-08-01 21:47:53 +0700

    many to one find & save

--- LearningBoot/src/main/java/com/fullSpring/LearningBoot/GoodsController.java
+++ LearningBoot/src/main/java/com/fullSpring/LearningBoot/GoodsController.java
@@ -1,28 +1,44 @@
 package com.fullSpring.LearningBoot;
 
 import com.fullSpring.LearningBoot.dto.GoodsDto;
+import com.fullSpring.LearningBoot.dto.InsertGoodsDto;
 import com.fullSpring.LearningBoot.entity.Goods;
+import com.fullSpring.LearningBoot.entity.Shop;
 import com.fullSpring.LearningBoot.repository.GoodsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/goods")
 public class GoodsController {
 
     @Autowired
     private GoodsRepository goodsRepository;
 
-    @GetMapping("/getGoods/{id}")
+    @GetMapping("/{id}")
     public GoodsDto getGoods(@PathVariable("id") String id) {
-       Goods goods = goodsRepository.findById(id).orElseThrow();
-       return new GoodsDto()
-               .setGoodsId(goods.getGoodsId())
-               .setGoodsName(goods.getGoodsName())
-               .setGoodsPrice(goods.getGoodsPrice())
-               .setShopId(goods.getShop().getShopId())
-               .setShopName(goods.getShop().getShopName());
+        Goods goods = goodsRepository.findById(id).orElseThrow();
+        return new GoodsDto()
+                .setGoodsId(goods.getGoodsId())
+                .setGoodsName(goods.getGoodsName())
+                .setGoodsPrice(goods.getGoodsPrice())
+                .setShopId(goods.getShop().getShopId())
+                .setShopName(goods.getShop().getShopName());
+    }
+
+    @PostMapping("/insertGoods")
+    public GoodsDto insertGoods(@RequestBody InsertGoodsDto insertGoodsDto) {
+        Goods goods = new Goods()
+                .setGoodsId(insertGoodsDto.getGoodsId())
+                .setGoodsName(insertGoodsDto.getGoodsName())
+                .setGoodsPrice(insertGoodsDto.getGoodsPrice())
+                .setShop(new Shop().setShopId(insertGoodsDto.getShopId()));
+        goods = goodsRepository.save(goods);
+        return new GoodsDto()
+                .setGoodsId(goods.getGoodsId())
+                .setGoodsName(goods.getGoodsName())
+                .setGoodsPrice(goods.getGoodsPrice())
+                .setShopId(goods.getShop().getShopId())
+                .setShopName(goods.getShop().getShopName());
     }
 }
