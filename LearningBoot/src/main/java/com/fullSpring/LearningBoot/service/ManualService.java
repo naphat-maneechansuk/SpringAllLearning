711187cbefc4bd545666918e31229d66e57bf288
naphat-maneechansuk <141388294+naphat-maneechansuk@users.noreply.github.com>
2025-08-06 22:08:13 +0700

    -มันเบสิคอะ ขอไม่เพิ่มเติมอะไรใน branch นี้เเล้วกัน ส่วนใหม่ที่เค้าสอนน่ะ

---
+++ LearningBoot/src/main/java/com/fullSpring/LearningBoot/service/ManualService.java
@@ -0,0 +1,20 @@
+package com.fullSpring.LearningBoot.service;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Accessors(chain = true)
+@Data
+public class ManualService {
+    private String name;
+    private String type;
+
+    public ManualService(String name, String type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    public void print(){
+        System.out.println("Service Name: " + name + " : "+ type);
+    }
+}
